c5d16cb8e32be38dc796e7286c5c7364ed505bdf
Mikael Zackrisson <mikza835@student.liu.se>
2015-09-30 16:54:50 +0100

    Fixed scene and camera. Created dog class

---
+++ SheepherdingAI/Source/SheepherdingAI/DogPawn.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SheepherdingAI.h"
+#include "DogPawn.h"
+
+
+// Sets default values
+ADogPawn::ADogPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set this pawn to be controlled by the lowest-numbered player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// Create a dummy root component we can attach things to.
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	// Create a camera and a visible object
+	//UCameraComponent* OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+	// Attach our camera and visible object to our root component. Offset and rotate the camera.
+	//OurCamera->AttachTo(RootComponent);
+	//OurCamera->SetRelativeLocation(FVector(-250.0f, 0.0f, 250.0f));
+	//OurCamera->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+	OurVisibleComponent->AttachTo(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ADogPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADogPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	// Handle movement based on our "MoveX" and "MoveY" axes
+	{
+		if (!CurrentVelocity.IsZero())
+		{
+			FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+			SetActorLocation(NewLocation);
+		}
+	}
+}
+
+// Called to bind functionality to input
+void ADogPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+	InputComponent->BindAxis("MoveX", this, &ADogPawn::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &ADogPawn::Move_YAxis);
+}
+
+void ADogPawn::Move_XAxis(float AxisValue)
+{
+	// Move at 100 units per second forward or backward
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void ADogPawn::Move_YAxis(float AxisValue)
+{
+	// Move at 100 units per second right or left
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
