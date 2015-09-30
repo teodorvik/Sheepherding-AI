0f81318bffe0a80289c75754d83fcf126c568210
teodorvik <teodor.johan.vik@gmail.com>
2015-09-30 11:43:57 +0200

    It has begun

---
+++ SheepherdingAI/Source/SheepherdingAI/SheepherdingAI.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SheepherdingAI.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SheepherdingAI, "SheepherdingAI" );
