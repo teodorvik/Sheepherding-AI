2d363bc723b5d6a87fd0397263b5172914a56b96
teodorvik <teodor.johan.vik@gmail.com>
2015-10-12 15:35:32 +0200

    AIDog

---
+++ SheepherdingAI/Source/SheepherdingAI/DogAIPawn.cpp
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SheepherdingAI.h"
+#include "DogAIPawn.h"
+
+
+// Sets default values
+ADogAIPawn::ADogAIPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+	OurVisibleComponent->AttachTo(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ADogAIPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// random weights 
+	// herdDistanceWeight;
+	// herdSpreadWeight;
+}
+
+// Called every frame
+void ADogAIPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	
+	// Get input
+	// All sheep
+	// goal
+	// distance from goal
+	float spread;	
+	spread = HerdSpread();
+
+
+}
+
+// Get how much the herd is seperated
+float ADogAIPawn::HerdSpread() {	
+	// Variables
+	std::vector<float> distance;
+	distance.reserve(sheepArray.Num());
+
+	FVector center = FVector(0.0f, 0.0f, 0.0f);
+
+	// Calculate distance from one sheep to all the others
+	// Sum and return
+	for (int i = 0; i < sheepArray.Num(); i++) {
+		float dist = 0.0f;
+		ASheepPawn* sheep = sheepArray[i];
+		center += sheep->GetActorLocation();
+
+		for (int j = 0; j < sheepArray.Num(); j++) {
+			if (i != j) {
+				FVector distVec = sheepArray[j]->GetActorLocation() - sheep->GetActorLocation();
+				dist += distVec.Size();
+			}			
+		}
+		distance[i] = dist;
+	}
+
+	float distanceTotal = 0.0f;
+	for (int i = 0; i < distance.size(); i++) {
+		distanceTotal += distance[i];
+	}
+
+	// Calculate center of the herd and update private variable. 
+	herdCenter = center/sheepArray.Num();
+
+	return distanceTotal;
+}
+
+// Herd distance from goal
+float ADogAIPawn::HerdDistanceToGoal() {
+	// Calculate center of herd to center of goal
+	float dist = herdCenter.Size();
+	return dist;
+}
+
+// Called to bind functionality to input
+void ADogAIPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
+void ADogAIPawn::SetSheepArray(TArray<class ASheepPawn*>& sheep){
+	sheepArray = sheep;
+}
---
+++ SheepherdingAI/Source/SheepherdingAI/DogAIPawn.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <vector>
+#include "SheepPawn.h"
+#include "SheepherdingAI.h"
+#include "GameFramework/Pawn.h"
+#include "DogAIPawn.generated.h"
+
+UCLASS()
+class SHEEPHERDINGAI_API ADogAIPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADogAIPawn();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+	float HerdSpread();
+	float HerdDistanceToGoal();
+
+	UPROPERTY(EditAnywhere)
+		USceneComponent* OurVisibleComponent;
+
+	// Store references to all sheep (called from blueprint)
+	UFUNCTION(BlueprintCallable, Category = "AIDog")
+		void SetSheepArray(UPARAM(ref) TArray<class ASheepPawn*> &sheep);
+
+private:
+	FVector herdCenter;
+	float herdDistanceWeight = 0.5f;
+	float herdSpreadWeight = 0.5f;
+	
+	TArray<class ASheepPawn*> sheepArray;
+};
